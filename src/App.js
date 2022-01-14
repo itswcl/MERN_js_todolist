2a72095c66f525d962f758a605a8160bff115a75
weicheng lee <z82111712@gmail.com>
2022-01-13 21:34:19 -0800

    update function create items

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import React, {useState} from 'react';
 import './App.css';
 import FormAdd from './components/FormAdd';
 
 function App() {
+  const [list, setList] = useState([
+    {title: "Get Python black belt.", done: false},
+  ])
+
+  const itemReceived = (itemTitle, done) => {
+    setList([
+      ...list,
+      {title: itemTitle, done: done}
+    ])
+  }
+
   return (
     <div className="App">
-      <FormAdd />
+      {JSON.stringify(list)}
+      <FormAdd onNewItem={itemReceived} />
     </div>
   );
 }
--- src/components/FormAdd.jsx
+++ src/components/FormAdd.jsx
@@ -1,9 +1,41 @@
-import React {useState} from 'react'
+import React, { useState } from 'react'
 
-const FormAdd = () => {
+const FormAdd = (props) => {
+    const [item, setItem] = useState({
+        title: "",
+        done: false,
+    })
+
+    const addItem = (e) => {
+        setItem({
+            title: e.target.value,
+            done: false,
+        })
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        props.onNewItem(item.title, item.done)
+        setItem({
+            ...item,
+            title: ""
+        })
+    }
+
     return (
-        <div>
-            <h1>HelloWorld</h1>
+        <div className='d-flex justify-content-center'>
+            <form onSubmit={handleSubmit} className='form-group'>
+                <div className='col'>
+                    <labe>Enter Item</labe>
+                    <input
+                    onChange={addItem}
+                    className='form-control'
+                    type='text'
+                    name='title'
+                    value={item.title}
+                    />
+                </div>
+                <button className="btn btn-success my-2">Add</button>
+            </form>
         </div>
     )
 }
